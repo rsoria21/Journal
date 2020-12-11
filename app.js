4eaaf04bb02af32f725d23c8ade2230e147f74ef
Ricardo Soria <ricardo@ricardos-mbp.attlocal.net>
2020-12-10 18:10:49 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Hey, wow so it my time to shine huh? Okay! Welcome, look around and explore. I hope you like what you see. Oh and yes those buttons work, press on them and explore what you can compose";
+const aboutContent = "Hey, Glad you made it this far. So I'm basically a place to keep your thoughts for the day.";
+const contactContent = "Whoa slow down there, this is a demo! Hey, but this is where he contact info would be.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
